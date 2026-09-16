208d5577f43d8420be5b261bf619bb3e96edf918
John D. Haughton <sloecomputer@gmail.com>
2026-09-16 20:51:36 +0100

    Add GPL2 licensing support

--- STB/ConsoleApp.h
+++ STB/ConsoleApp.h
@@ -18,122 +18,6 @@ namespace STB {
 //! A generic class to simplify the creation of user friendly console applications
 class ConsoleApp
 {
-private:
-#if !defined(PDK_NCONSOLE)
-   Option<bool> opt_version{'v', "version", "Display version information"};
-   Option<bool> opt_help{   'h', "help",    "Display this help"};
-
-   static const char* extractFilename(const char* path)
-   {
-      // Windows is not supported
-      const char* filename = strrchr(path, '/');
-
-      return filename != nullptr ? filename + 1
-                                 : path;
-   }
-
-   static void normal() { printf("\033[0m"); }
-   static void bold()   { printf("\033[1m"); }
-
-   void showVersion()
-   {
-      printf("\n");
-      printf("Program      : %s\n", program);
-      printf("Description  : %s\n", description);
-      printf("Author       : %s\n", author);
-      printf("Version      : %s\n", PDK_PROJ_VERSION);
-      if (link != nullptr)
-      {
-         printf("Link         : %s\n", link);
-      }
-      printf("Commit       : %s\n", PDK_PROJ_COMMIT);
-      printf("Built        : %s %s\n", __TIME__, __DATE__);
-      printf("Compiler     : %s\n", __VERSION__);
-#if defined(__arm__)
-      printf("Architecture : ARM-32\n");
-#elif defined(__aarch64__)
-      printf("Architecture : ARM-64\n");
-#elif defined(__mips__)
-      printf("Architecture : MIPS\n");
-#elif defined(__powerpc__)
-      printf("Architecture : PowerPC\n");
-#elif defined(__sparc__)
-      printf("Architecture : SPARC\n");
-#elif defined(__i386__)
-      printf("Architecture : x86-32\n");
-#elif defined(__amd64__)
-      printf("Architecture : AMD-64 (x86-64)\n");
-#endif
-      printf("\n");
-      bold();
-      printf("Copyright (c) %s %s\n", copyright_year, author);
-      normal();
-      printf("\n");
-      printf("%s", MIT_LICENSE);
-      printf("\n");
-      printf("\n");
-
-      exit(0);
-   }
-
-   void showHelp()
-   {
-      printf("\n");
-      bold();
-      printf("NAME\n");
-      normal();
-      printf("     %s - %s\n", program, description);
-      printf("\n");
-      bold();
-      printf("SYNOPSIS\n");
-      normal();
-      printf("     %s [options] %s\n", name, OptionBase::getSynopsisSuffix());
-      printf("\n");
-      bold();
-      printf("OPTIONS\n");
-      normal();
-
-      OptionBase::printHelpAll();
-
-      printf("\n");
-
-      showExtraHelp();
-
-      exit(0);
-   }
-#endif
-
-protected:
-#if !defined(PDK_NCONSOLE)
-   const char* name;
-   const char* program;
-   const char* description;
-   const char* link;
-   const char* author;
-   const char* copyright_year;
-#endif
-
-   void error(const char* format, ...)
-   {
-#if !defined(PDK_NCONSOLE)
-      va_list ap;
-
-      fprintf(stderr, "%s: ERROR - ", name);
-
-      va_start(ap, format);
-      vfprintf(stderr, format, ap);
-      va_end(ap);
-
-      fprintf(stderr, "\n");
-#endif
-
-      exit(1);
-   }
-
-   virtual void showExtraHelp() {}
-
-   virtual int startConsoleApp() = 0;
-
 public:
    ConsoleApp(const char* program_,
               const char* description_,
@@ -150,6 +34,8 @@ public:
       , copyright_year(copyright_year_)
 #endif
    {}
+
+   void licGPL2() { lic_gpl2 = true; }
 
    int parseArgsAndStart(int argc, const char* argv[])
    {
@@ -204,6 +90,125 @@ public:
 
       return startConsoleApp();
    }
+
+protected:
+#if !defined(PDK_NCONSOLE)
+   const char* name;
+   const char* program;
+   const char* description;
+   const char* link;
+   const char* author;
+   const char* copyright_year;
+#endif
+
+   void error(const char* format, ...)
+   {
+#if !defined(PDK_NCONSOLE)
+      va_list ap;
+
+      fprintf(stderr, "%s: ERROR - ", name);
+
+      va_start(ap, format);
+      vfprintf(stderr, format, ap);
+      va_end(ap);
+
+      fprintf(stderr, "\n");
+#endif
+
+      exit(1);
+   }
+
+   virtual void showExtraHelp() {}
+
+   virtual int startConsoleApp() = 0;
+
+private:
+#if !defined(PDK_NCONSOLE)
+   Option<bool> opt_version{'v', "version", "Display version information"};
+   Option<bool> opt_help{   'h', "help",    "Display this help"};
+
+   static const char* extractFilename(const char* path)
+   {
+      // Windows is not supported
+      const char* filename = strrchr(path, '/');
+
+      return filename != nullptr ? filename + 1
+                                 : path;
+   }
+
+   static void normal() { printf("\033[0m"); }
+   static void bold()   { printf("\033[1m"); }
+
+   void showVersion()
+   {
+      printf("\n");
+      printf("Program      : %s\n", program);
+      printf("Description  : %s\n", description);
+      printf("Author       : %s\n", author);
+      printf("Version      : %s\n", PDK_PROJ_VERSION);
+      if (link != nullptr)
+      {
+         printf("Link         : %s\n", link);
+      }
+      printf("Commit       : %s\n", PDK_PROJ_COMMIT);
+      printf("Built        : %s %s\n", __TIME__, __DATE__);
+      printf("Compiler     : %s\n", __VERSION__);
+#if defined(__arm__)
+      printf("Architecture : ARM-32\n");
+#elif defined(__aarch64__)
+      printf("Architecture : ARM-64\n");
+#elif defined(__mips__)
+      printf("Architecture : MIPS\n");
+#elif defined(__powerpc__)
+      printf("Architecture : PowerPC\n");
+#elif defined(__sparc__)
+      printf("Architecture : SPARC\n");
+#elif defined(__i386__)
+      printf("Architecture : x86-32\n");
+#elif defined(__amd64__)
+      printf("Architecture : AMD-64 (x86-64)\n");
+#endif
+      printf("\n");
+      bold();
+      printf("Copyright (c) %s %s\n", copyright_year, author);
+      normal();
+      printf("\n");
+      if (lic_gpl2) printf("%s", GPL2_LICENSE);
+      else          printf("%s", MIT_LICENSE);
+      printf("\n");
+      printf("\n");
+
+      exit(0);
+   }
+
+   void showHelp()
+   {
+      printf("\n");
+      bold();
+      printf("NAME\n");
+      normal();
+      printf("     %s - %s\n", program, description);
+      printf("\n");
+      bold();
+      printf("SYNOPSIS\n");
+      normal();
+      printf("     %s [options] %s\n", name, OptionBase::getSynopsisSuffix());
+      printf("\n");
+      bold();
+      printf("OPTIONS\n");
+      normal();
+
+      OptionBase::printHelpAll();
+
+      printf("\n");
+
+      showExtraHelp();
+
+      exit(0);
+   }
+#endif
+
+   bool lic_gpl2{false};
 };
 
 } // namespace STB
--- STB/License.h
+++ STB/License.h
@@ -49,3 +49,16 @@
    "OTHER DEALINGS IN THE SOFTWARE.\n"                                                             \
    "\n"                                                                                            \
    "For more information, please refer to <https://unlicense.org>\n"
+
+#define GPL2_LICENSE                                                                               \
+  "This program is free software; you can redistribute it and/or modify\n"                         \
+  "it under the terms of the GNU General Public License version 2 as\n"                            \
+  "published by the Free Software Foundation.\n"                                                   \
+  "\n"                                                                                             \
+  "This program is distributed in the hope that it will be useful,\n"                              \
+  "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"                               \
+  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\n"                                 \
+  "GNU General Public License version 2 for more details.\n"                                       \
+  "\n"                                                                                             \
+  "You should have received a copy of the GNU General Public License\n"                            \
+  "along with this program. If not, see <https://www.gnu.org/licenses/>."
